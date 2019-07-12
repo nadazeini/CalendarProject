1ad5174a806c31adde6f0b405d87ae1b0fcce14c
shuang pan <panshuangis88188@gmail.com>
2019-07-11 22:23:00 -0700

    second commit

--- CalendarTester.java
+++ CalendarTester.java
@@ -1,16 +1,8 @@
 package calendarProject;
 
-import java.awt.Container;
-
-import javax.swing.BoxLayout;
-import javax.swing.JFrame;
-
 public class CalendarTester {
 	public static void main(String[] args) {
 		CalendarFrame calendarFrame = new CalendarFrame();
-	    
-		JFrame frame2 = new JFrame();
-	    final Container contentPane2 = frame2.getContentPane();
-	    frame2.setLayout(new BoxLayout(contentPane2, BoxLayout.Y_AXIS));
+		EventFrame eventFrame = new EventFrame(calendarFrame);
 	}
 }
--- EventFrame.java
+++ EventFrame.java
@@ -1,7 +1,81 @@
 package calendarProject;
 
+import java.awt.Container;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 public class EventFrame extends JFrame{
+	
+	public EventFrame(CalendarFrame calendarFrame) {
+	    final Container contentPane = getContentPane();
+	    setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
+	    
+	    JButton dayButton = new JButton("Day");
+	    JButton weekButton = new JButton("Week");
+	    JButton monthButton = new JButton("Month");
+	    JButton agendaButton = new JButton("Agenda");
+	    final JButton fileButton = new JButton("From File");
+		fileButton.addMouseListener(new MouseListener() {
 
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				fileButton.setBorderPainted(false);
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
+	    
+	    JPanel panel1 = new JPanel();
+	    panel1.add(dayButton);
+	    panel1.add(weekButton);
+	    panel1.add(monthButton);
+	    panel1.add(agendaButton);
+	    
+	    JPanel panel2 = new JPanel();
+	    panel2.add(fileButton);
+	    
+	    final int row = 30;
+	    final int column = 50;
+	    final JTextArea textArea = new JTextArea(row, column);
+	    
+	    JPanel panel3 = new JPanel();
+	    panel3.add(textArea);
+	    
+	    add(panel1);
+	    add(panel2);
+	    add(panel3);
+	    
+	    setLocation(550, 0);
+	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    pack();
+	    setVisible(true);
+	}
 }
