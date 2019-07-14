ac61535cfc49f505fef9ced5b1eaab640ed7abef
shuang pan <panshuangis88188@gmail.com>
2019-07-14 14:03:07 -0700

    Final version for creating events

--- src/calendarProject/CreateEventFrame.java
+++ src/calendarProject/CreateEventFrame.java
@@ -1,5 +1,207 @@
 package calendarProject;
 
-public class CreateEventFrame {
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.time.LocalDate;
+import java.util.ArrayList;
 
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class CreateEventFrame extends JFrame{
+	private LocalDate firstDay;
+	private LocalDate click;
+	private CreateEventFrame cef;
+	private CalendarFrame cf;
+	private JButton date;
+	public static final String DAY_OF_WEEK = "SMTWTFA";
+	
+	private static final long serialVersionUID = 1L;
+
+	public CreateEventFrame(LocalDate click, CalendarFrame cf, JButton date) {
+		this.cf = cf;
+		this.click = click;
+		firstDay = LocalDate.of(click.getYear(), click.getMonth(), 1);
+		this.date = date;
+	
+		JButton dateButton = new JButton();
+		JButton nextMonthButton = new JButton("Next Month");
+		JButton previousMonthButton = new JButton("Previous Month");
+		
+	    final Container contentPane = getContentPane();
+	    setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
+		
+		JPanel panel3 = new JPanel();
+		dateButton.setText(firstDay.getMonth().toString() + " " + Integer.toString(firstDay.getYear()));
+		dateButton.setBorderPainted(false);
+		panel3.add(dateButton);
+		panel3.add(previousMonthButton);
+		panel3.add(nextMonthButton);
+		
+		JPanel panel4 = new JPanel();
+		panel4.setLayout(new GridLayout(7, 7));
+		
+		setDate(panel4, dateButton);
+		
+		ActionListener eventListener = new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(((JButton)e.getSource()) == previousMonthButton) {
+					firstDay = firstDay.minusMonths(1);
+					firstDay = LocalDate.of(firstDay.getYear(), firstDay.getMonth(), 1);
+				}
+				else if(((JButton)e.getSource()) == nextMonthButton) {
+					firstDay = firstDay.plusMonths(1);
+					firstDay = LocalDate.of(firstDay.getYear(), firstDay.getMonth(), 1);
+				}
+				setDate(panel4, dateButton);
+			}
+			
+		};
+		previousMonthButton.addActionListener(eventListener);
+		nextMonthButton.addActionListener(eventListener);
+		
+		add(panel3);
+		add(panel4);
+		
+		setTitle("Choose date");
+		setLocation(650, 350);
+	    setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+	    pack();
+	    setVisible(true);
+	}
+
+	public void setDate(JPanel panel4, JButton dateButton) {
+		panel4.removeAll();
+		for(int i = 0; i < DAY_OF_WEEK.length(); i++) {
+			JButton a = new JButton(DAY_OF_WEEK.substring(i, i + 1));
+			a.setBorderPainted(false);
+			panel4.add(a);
+		}
+		ArrayList<JButton> buttons = new ArrayList<>();
+		int firstDayOfMonth = firstDay.getDayOfWeek().getValue();
+		if(firstDayOfMonth != 7) {
+			for(int i = 0; i < firstDayOfMonth; i++) {
+				firstDay = firstDay.minusDays(1);
+			}
+			for(int i = 0; i < firstDayOfMonth; i++) {
+				JButton a = new JButton(Integer.toString(firstDay.getDayOfMonth()));
+				a.setForeground(Color.GRAY);
+				a.setBorderPainted(false);
+				panel4.add(a);
+				firstDay = firstDay.plusDays(1);
+				buttons.add(a);
+			}
+		}
+		int currentMonthLength = firstDay.getMonth().length(firstDay.isLeapYear());
+		for(int i = 0; i < currentMonthLength; i++) {
+			JButton a = new JButton(Integer.toString(firstDay.getDayOfMonth()));
+			if(firstDay.equals(LocalDate.now())) {
+				a.setForeground(Color.RED);
+				a.setFont(new Font("ROMAN_BASELINE", Font.ITALIC, 12));
+			}
+			else {
+				a.setBorderPainted(false);
+			}
+			panel4.add(a);
+			firstDay = firstDay.plusDays(1);
+			buttons.add(a);
+		}
+		int leftDays = 0;
+		if(firstDayOfMonth != 7)
+			leftDays = 49 - currentMonthLength - firstDayOfMonth - 7;
+		else
+			leftDays = 49 - currentMonthLength - 7;
+		for(int i = 0; i < leftDays; i++) {
+			JButton a = new JButton(Integer.toString(firstDay.getDayOfMonth()));
+			a.setForeground(Color.GRAY);
+			a.setBorderPainted(false);
+			panel4.add(a);
+			firstDay = firstDay.plusDays(1);
+			buttons.add(a);
+		}
+		
+		firstDay = firstDay.minusMonths(1);
+		dateButton.setText(firstDay.getMonth().toString() + " " + Integer.toString(firstDay.getYear()));
+		if(!click.equals(LocalDate.now())) {
+			if(firstDay.getYear() == click.getYear() 
+			&& firstDay.getMonthValue() == click.getMonthValue()) {
+				for(JButton button: buttons) {
+					if(button.getForeground().equals(Color.BLACK) 
+					&& Integer.parseInt(button.getText()) == click.getDayOfMonth()) {
+						button.setForeground(Color.BLUE);
+						button.setBorderPainted(true);
+						break;
+					}
+				}
+			}
+		}
+		
+		for(JButton button: buttons) {
+			button.addMouseListener(new MouseListener() {
+
+				@Override
+				public void mouseClicked(MouseEvent e) {
+					if(!button.getForeground().equals(Color.GRAY)) {
+						click = LocalDate.of(firstDay.getYear(), firstDay.getMonth(), Integer.parseInt(button.getText()));
+					}
+					else {
+						int day = Integer.parseInt(button.getText());
+						if(day > 22) {
+							firstDay = firstDay.minusMonths(1);
+						}
+						else {
+							firstDay = firstDay.plusMonths(1);
+						}
+						firstDay = LocalDate.of(firstDay.getYear(), firstDay.getMonth(), 1);
+						click = LocalDate.of(firstDay.getYear(), firstDay.getMonth(), Integer.parseInt(button.getText()));
+					}
+
+					cf.setCurrentClick(click);
+					cf.setFirstDay(firstDay);
+					cf.setDate();
+					cf.getEventFrame().stateChanged(null);
+					date.setText(click.toString());
+					cef.dispose();
+				}
+
+				@Override
+				public void mousePressed(MouseEvent e) {
+
+				}
+
+				@Override
+				public void mouseReleased(MouseEvent e) {
+
+				}
+
+				@Override
+				public void mouseEntered(MouseEvent e) {
+					button.setBorderPainted(true);
+				}
+
+				@Override
+				public void mouseExited(MouseEvent e) {
+					if(!button.getForeground().equals(Color.BLUE))
+						button.setBorderPainted(false);
+				}
+				
+			});
+		}
+		panel4.revalidate();
+		panel4.repaint();
+	}
+	
+	public void setCloseReference(CreateEventFrame cef) {
+		this.cef = cef;
+	}
 }
