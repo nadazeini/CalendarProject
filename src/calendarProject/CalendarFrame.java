eb1f58ad90ed7d5aeca64f99ad13ecf3d4c0da15
shuang pan <panshuangis88188@gmail.com>
2019-07-13 18:29:39 -0700

    Compare firstDay and Local.now() directly

--- src/calendarProject/CalendarFrame.java
+++ src/calendarProject/CalendarFrame.java
@@ -187,9 +187,7 @@ public class CalendarFrame extends JFrame{
 		int currentMonthLength = firstDay.getMonth().length(firstDay.isLeapYear());
 		for(int i = 0; i < currentMonthLength; i++) {
 			JButton a = new JButton(Integer.toString(firstDay.getDayOfMonth()));
-			if(firstDay.getYear() == LocalDate.now().getYear() 
-			&& firstDay.getMonthValue() == LocalDate.now().getMonthValue() 
-			&& firstDay.getDayOfMonth() == LocalDate.now().getDayOfMonth()) {
+			if(firstDay.equals(LocalDate.now()) {
 				a.setForeground(Color.RED);
 				a.setFont(new Font("ROMAN_BASELINE", Font.ITALIC, 12));
 			}
