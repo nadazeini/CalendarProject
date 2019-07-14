08e5902ce739be3639aa3f60256588049e3b74e8
shuang pan <panshuangis88188@gmail.com>
2019-07-13 17:34:23 -0700

    Formatter for formating events

---
+++ src/calendarProject/SimpleFormatter.java
@@ -0,0 +1,25 @@
+package calendarProject;
+
+public class SimpleFormatter implements EventFormatter{
+
+	@Override
+	public String formatHeader() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String formatEvent(Event event) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String formatFooter() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
+
+
