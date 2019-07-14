b557d424685d5565a00a41d35df5026790e9b207
shuang pan <panshuangis88188@gmail.com>
2019-07-13 17:58:01 -0700

    Update textArea after adding events from file

--- src/calendarProject/EventFrame.java
+++ src/calendarProject/EventFrame.java
@@ -1,20 +1,31 @@
 package calendarProject;
 
 import java.awt.Container;
-import java.awt.Desktop;
-import java.awt.event.*;
+import java.awt.FileDialog;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
-import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
-public class EventFrame extends JFrame{
+public class EventFrame extends JFrame implements ChangeListener{
+	private DataModel dataModel;
+	private CalendarFrame calendarFrame;
 	
-	public EventFrame(CalendarFrame calendarFrame) {
+	private static final long serialVersionUID = 1L;
+	
+	public EventFrame(DataModel dataModel) {
+		this.dataModel = dataModel;
 	    final Container contentPane = getContentPane();
 	    setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
 	    
@@ -22,40 +33,36 @@ public class EventFrame extends JFrame{
 	    JButton weekButton = new JButton("Week");
 	    JButton monthButton = new JButton("Month");
 	    JButton agendaButton = new JButton("Agenda");
-	    final JButton fileButton = new JButton("From File"); //why final?
-	fileButton.addMouseListener(new MouseListener() {
-
+	    JButton fileButton = new JButton("From File");
+	    ActionListener listener = new ActionListener() {
 			@Override
-			public void mouseClicked(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
+			public void actionPerformed(ActionEvent e) {
+				FileDialog fileDialog = new FileDialog(new JFrame());
+				fileDialog.setVisible(true);
+				File[] files = fileDialog.getFiles();
+				Scanner scanner = null;
+				if(files.length > 0) {
+					try {
+				    	scanner = new Scanner(files[0]);
+				    	processFile(scanner);
+					}
+					catch (FileNotFoundException exception) {
+					    exception.printStackTrace();
+					} 
+					finally {
+					    if (scanner != null) {
+					        scanner.close();
+					    }
+					}
+					fileButton.setBorderPainted(false);
+					fileButton.removeActionListener(fileButton.getActionListeners()[0]);
+				}
 			}
-
-			@Override
-			public void mousePressed(MouseEvent e) {
-				
-			}
-
-			@Override
-			public void mouseReleased(MouseEvent e) {
-				fileButton.setBorderPainted(false);
-			}
-
-			@Override
-			public void mouseEntered(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void mouseExited(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-		}); 
-		
-	    fileButton.addActionListener(new ActionListener() {
+	    };
+	    
+	    fileButton.addActionListener(listener);
+	    
+	/*    fileButton.addActionListener(new ActionListener() {
 	    	public void actionPerformed(ActionEvent e) {
 	    		if (Desktop.isDesktopSupported()) {
 	    		    try {
@@ -69,7 +76,8 @@ public class EventFrame extends JFrame{
 					}
 	    		}
 	    	}
-	    });
+	    });  */
+	    
 	    JPanel panel1 = new JPanel();
 	    panel1.add(dayButton);
 	    panel1.add(weekButton);
@@ -82,6 +90,7 @@ public class EventFrame extends JFrame{
 	    final int row = 30;
 	    final int column = 50;
 	    final JTextArea textArea = new JTextArea(row, column);
+	    textArea.setEditable(false);
 	    
 	    JPanel panel3 = new JPanel();
 	    panel3.add(textArea);
@@ -90,9 +99,35 @@ public class EventFrame extends JFrame{
 	    add(panel2);
 	    add(panel3);
 	    
-	    setLocation(550, 0);
+	    setLocation(0, 500);
 	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    pack();
 	    setVisible(true);
+	}
+	
+	public void processFile(Scanner fileScanner) {
+		while(fileScanner.hasNextLine()) {
+			String events[] = fileScanner.nextLine().split(";");
+			dataModel.addEvent(new Event(events[0], Integer.parseInt(events[1]), Integer.parseInt(events[2]), Integer.parseInt(events[3])
+					   				   , events[4], Integer.parseInt(events[5]), Integer.parseInt(events[6])));
+		}
+	}
+	
+	public void setCalendarFrame(CalendarFrame frame) {
+		this.calendarFrame = frame;
+	}
+
+	@Override
+	public void stateChanged(ChangeEvent e) {
+		LocalDate current = calendarFrame.getCurrentClick();
+		ArrayList<Event> events = dataModel.getEvents();
+		ArrayList<Event> eventsOnCurrentDay = new ArrayList<>();
+		for(int i = 0; i < events.size(); i++) {
+			Event event = events.get(i);
+			if(current.getYear() == event.getYear() 
+		   && (current.getMonthValue() >= event.getStartingMonth() && current.getMonthValue() < event.getEndingMonth())
+		   && event.getDays().contains(current.getDayOfWeek().getValue()))
+				eventsOnCurrentDay.add(event);
+		}
 	}
 }
