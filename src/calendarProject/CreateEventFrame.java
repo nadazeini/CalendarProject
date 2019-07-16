ff6de2c74b5c861d617e91d5b9fa564c36dd284c
shuang pan <panshuangis88188@gmail.com>
2019-07-16 11:28:26 -0700

    A empty create event frame

---
+++ src/calendarProject/CreateEventFrame.java
@@ -0,0 +1,29 @@
+package calendarProject;
+
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowFocusListener;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class CreateEventFrame extends JFrame{
+
+}
