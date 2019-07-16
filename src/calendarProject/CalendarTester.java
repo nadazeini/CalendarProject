ef79c457c67a5e3e0b40eda1907c0c1dd9667319
shuang pan <panshuangis88188@gmail.com>
2019-07-15 20:14:21 -0700

    add reference to itself for creating event

--- src/calendarProject/CalendarTester.java
+++ src/calendarProject/CalendarTester.java
@@ -2,13 +2,16 @@ package calendarProject;
 
 public class CalendarTester {
 	public static void main(String[] args) {
+		EventFormatter formatter = new SimpleFormatter(); 
+		
 		DataModel dataModel = new DataModel();
 		
-		EventFrame eventFrame = new EventFrame(dataModel);
+		EventFrame eventFrame = new EventFrame(dataModel, formatter);
 		CalendarFrame calendarFrame = new CalendarFrame(dataModel);
 		
 		eventFrame.setCalendarFrame(calendarFrame);
 		calendarFrame.setEventFrame(eventFrame);
+		calendarFrame.setCalendarReference(calendarFrame);
 		
 		dataModel.addChangeListener(eventFrame);
 	}
