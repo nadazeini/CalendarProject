1ab546440e27a96819446904f5238df85af7d090
shuang pan <panshuangis88188@gmail.com>
2019-07-16 16:56:57 -0700

    Simpler way to format event

--- src/calendarProject/EventFormatter.java
+++ src/calendarProject/EventFormatter.java
@@ -1,7 +1,9 @@
 package calendarProject;
 
+import java.time.LocalDate;
+
 public interface EventFormatter {
-	String formatHeader();
+	String formatHeader(LocalDate click);
 	String formatEvent(Event event);
 	String formatFooter();
 }
--- src/calendarProject/SimpleFormatter.java
+++ src/calendarProject/SimpleFormatter.java
@@ -1,10 +1,22 @@
 package calendarProject;
 
+import java.time.LocalDate;
+
 public class SimpleFormatter implements EventFormatter{
 	@Override
+	public String formatHeader(LocalDate click) {
+		return click.toString() + '\n';
+	}
+	
+	@Override
 	public String formatEvent(Event event) {
-		return event.getStartingTime() + " - " 
-			 + event.getEndingTime() + ": " + event.getEventName() + '\n';
+		return event.getStartingTime() + ":00 - "
+			 + event.getEndingTime() + ":00: " + event.getEventName() + '\n';
+	}
+
+	@Override
+	public String formatFooter() {
+		return "\n";
 	}
 }
 
