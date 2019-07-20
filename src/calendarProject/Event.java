c983c8848493dd9d7fbb4a6576a3c20590b3532a
chenyr9712 <52802551+chenyr9712@users.noreply.github.com>
2019-07-20 00:54:44 -0700

    Update Event.java

--- src/calendarProject/Event.java
+++ src/calendarProject/Event.java
@@ -4,97 +4,111 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Event implements Comparable<Event>{
-	public static Map<String, Integer> DAYS_AND_WEEK = new HashMap<>() {
-		private static final long serialVersionUID = 1L;
-	{
-		put("M", 1);
-		put("T", 2);
-		put("W", 3);
-		put("R", 4);
-		put("F", 5);
-		put("A", 6);
-		put("S", 7);
-	}};
-	public static Map<Integer, String> WEEK_AND_DAYS  = new HashMap<>() {
-		private static final long serialVersionUID = 1L;
-	{
-		put(1, "M");
-		put(2, "T");
-		put(3, "W");
-		put(4, "R");
-		put(5, "F");
-		put(6, "A");
-		put(7, "S");
-	}};
-	private String eventName;
-	private int year;
-	private int startingMonth;
-	private int endingMonth;
-	private ArrayList<Integer> days;
-	private int startingTime;
-	private int endingTime;
-	
-	public Event(String eventName, int year, int startingMonth, int endingMonth,
-				 String dayOfWeek, int startingTime, int endingTime) {
-		days = new ArrayList<>();
-		this.eventName = eventName;
-		this.year = year;
-		this.startingMonth = startingMonth;
-		this.endingMonth = endingMonth;
-		if(endingMonth == 0) {
-			days.add(Integer.parseInt(dayOfWeek));
-		}
-		else {
-			for(int i = 0; i < dayOfWeek.length(); i++) {
-				days.add(DAYS_AND_WEEK.get(dayOfWeek.substring(i, i + 1)));
-			}
-		}
-		this.startingTime = startingTime;
-		this.endingTime = endingTime;
-	}
-	
-	public String getEventName() {
-		return eventName;
-	}
-	
-	public int getYear() {
-		return year;
-	}
-	
-	public int getStartingMonth() {
-		return startingMonth;
-	}
-	
-	public int getEndingMonth() {
-		return endingMonth;
-	}
-	
-	public ArrayList<Integer> getDays() {
-		return days;
-	}
-	
-	public int getStartingTime() {
-		return startingTime;
-	}
-	
-	public int getEndingTime() {
-		return endingTime;
-	}
+public class Event implements Comparable<Event> {
+    public static final Map<String, Integer> daysAndWeek = new HashMap<>();
+    private String eventName;
+    private int year;
+    private int startingMonth;
+    private int endingMonth;
+    private ArrayList<Integer> days = new ArrayList<>();
+    private int startingTime;
+    private int endingTime;
 
-	@Override
-	public int compareTo(Event event) {
-		if(startingTime < event.startingTime)
-			return -1;
-		else if(startingTime > event.startingTime)
-			return 1;
-		else {
-			if(endingTime < event.endingTime)
-				return -1;
-			else if(endingTime > event.endingTime)
-				return 1;
-			else
-				return 0;
-		}
-	}
+    public Event(String eventName, int year, int startingMonth, int endingMonth,
+                 String dayOfWeek, int startingTime, int endingTime) {
+        daysAndWeek.put("M", 1);
+        daysAndWeek.put("T", 2);
+        daysAndWeek.put("W", 3);
+        daysAndWeek.put("R", 4);
+        daysAndWeek.put("F", 5);
+        daysAndWeek.put("A", 6);
+        daysAndWeek.put("S", 7);
+        this.eventName = eventName;
+        this.year = year;
+        this.startingMonth = startingMonth;
+        this.endingMonth = endingMonth;
+        for (int i = 0; i < dayOfWeek.length(); i++) {
+            days.add(daysAndWeek.get(dayOfWeek.substring(i, i + 1)));
+        }
+        this.startingTime = startingTime;
+        this.endingTime = endingTime;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getStartingMonth() {
+        return startingMonth;
+    }
+
+    public int getEndingMonth() {
+        return endingMonth;
+    }
+
+    public ArrayList<Integer> getDays() {
+        return days;
+    }
+
+    public int startingTime() {
+        return startingTime;
+    }
+
+    public int endingTime() {
+        return endingTime;
+    }
+
+    @Override
+    public int compareTo(Event event) {
+        int compareYear = Integer.compare(year, event.year);
+        if (compareYear < 0)
+            return -1;
+        else if (compareYear > 0)
+            return 1;
+        else {
+            int compareStartingMonth = Integer.compare(startingMonth, event.startingMonth);
+            if (compareStartingMonth < 0)
+                return -1;
+            else if (compareStartingMonth > 0)
+                return 1;
+            else {
+                int compareEndingMonth = Integer.compare(endingMonth, event.endingMonth);
+                if (compareEndingMonth < 0)
+                    return -1;
+                else if (compareStartingMonth > 0)
+                    return 1;
+                else {
+                    int compareDayOfWeek = Integer.compare(days.get(0), days.get(0));
+                    if (compareDayOfWeek < 0)
+                        return -1;
+                    else if (compareDayOfWeek > 0)
+                        return 1;
+                    else {
+                        int compareStartingTime = Integer.compare(startingTime, event.startingTime);
+                        if (compareStartingTime < 0)
+                            return -1;
+                        else if (compareStartingTime > 0)
+                            return 1;
+                        else {
+                            int compareEndingTime = Integer.compare(endingTime, event.endingTime);
+                            if (compareEndingTime < 0)
+                                return -1;
+                            else if (compareEndingTime > 0)
+                                return 1;
+                            else
+                                return 0;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public boolean conflict(Event e) {
+        return true;
+    }
 }
