380effb44d332ad6e07ea949bb32a83b56aca367
chenyr9712 <52802551+chenyr9712@users.noreply.github.com>
2019-07-20 00:53:30 -0700

    Update CalendarTester.java

--- src/calendarProject/CalendarTester.java
+++ src/calendarProject/CalendarTester.java
@@ -1,18 +1,13 @@
 package calendarProject;
 
 public class CalendarTester {
-	public static void main(String[] args) {
-		EventFormatter formatter = new SimpleFormatter(); 
-		
-		DataModel dataModel = new DataModel();
-		
-		EventFrame eventFrame = new EventFrame(dataModel, formatter);
-		CalendarFrame calendarFrame = new CalendarFrame(dataModel);
-		
-		eventFrame.setCalendarFrame(calendarFrame);
-		calendarFrame.setEventFrame(eventFrame);
-		calendarFrame.setCalendarReference(calendarFrame);
-		
-		dataModel.addChangeListener(eventFrame);
-	}
+    public static void main(String[] args) {
+  
+        DataModel dataModel = new DataModel();
+        EventFrame eventFrame = new EventFrame(dataModel);
+        CalendarFrame calendarFrame = new CalendarFrame(dataModel);
+        eventFrame.setCalendarFrame(calendarFrame);
+        calendarFrame.setEventFrame(eventFrame);
+        dataModel.addChangeListener(eventFrame);
+    }
 }
