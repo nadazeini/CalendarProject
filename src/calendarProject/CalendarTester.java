d86c2afe88043f89043376d7830816a5b805bb12
Nada ElZeini <47260563+nadazeini@users.noreply.github.com>
2019-10-14 18:49:37 -0700

    final update

--- src/calendarProject/CalendarTester.java
+++ src/calendarProject/CalendarTester.java
@@ -1,23 +1,25 @@
-package calendarProject;
-
-import java.time.LocalDate;
-import java.util.HashMap;
-
-
+/**
+ * A calendar program.
+ * @author Shuang Pan, Yunru Chen, Nada Elzeini
+ * @version 1.0 07/23/2019
+ */
 public class CalendarTester {
-
-
+	/**
+	 * Initialize the calendar and event frame.
+	 * @param args unused
+	 */
 	public static void main(String[] args) {
+		EventFormatter formatter = new SimpleFormatter(); 
 		
-		HashMap<LocalDate, Event> hashMap = new HashMap<>();
-		 DataModel dataModel = new DataModel(hashMap);
+		DataModel dataModel = new DataModel();
 		
-
-	        EventFrame eventFrame = new EventFrame();
-	        CalendarFrame calendarFrame = new CalendarFrame(dataModel);
-	        eventFrame.setCalendarFrame(calendarFrame);
-	        calendarFrame.setEventFrame(eventFrame);
-	        dataModel.addChangeListener(eventFrame);
+		EventFrame eventFrame = new EventFrame(dataModel, formatter);
+		CalendarFrame calendarFrame = new CalendarFrame(dataModel);
+		
+		eventFrame.setCalendarFrame(calendarFrame);
+		calendarFrame.setEventFrame(eventFrame);
+		calendarFrame.setCalendarReference(calendarFrame);
+		
+		dataModel.addChangeListener(eventFrame);
 	}
-
 }
