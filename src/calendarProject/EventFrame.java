c0fca6742159b867aa321bbf21b6076dd76cf0e7
Nada ElZeini <47260563+nadazeini@users.noreply.github.com>
2019-10-14 18:50:56 -0700

    Update EventFrame.java

--- src/calendarProject/EventFrame.java
+++ src/calendarProject/EventFrame.java
@@ -1,287 +1,342 @@
-package calendarProject;
-
-import java.awt.*;
+import java.awt.Container;
+import java.awt.FileDialog;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.text.ParseException;
 import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.YearMonth;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.HashMap;
 import java.util.Scanner;
 
-import javax.swing.*;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
-
-// The main time interface, we must share the events and partial data of the two interfaces through one module
-public class EventFrame extends JFrame implements ChangeListener {
-	
+/**
+ * The interface of the event frame.
+ * @author Shuang Pan, Yunru Chen, Nada Elzeini
+ * @version 1.0 07/23/2019
+ */
+public class EventFrame extends JFrame implements ChangeListener{
+	private DataModel dataModel;
 	private CalendarFrame calendarFrame;
-	// private static final long serialVersionUID = 1L;
-	private JPanel panel3 = new JPanel();
-	private final int row = 300;
-	private final int column = 500;
-
-	private static HashMap<LocalDate, Event> hashMap = new HashMap<>();
-	private static DataModel dataModel = new DataModel(hashMap);
-
-	public EventFrame() {
+	private EventFormatter formatter;
+	private final JTextArea textArea;
+	private String view;
+	private LocalDate current;
+	private String result;
+	private LocalDate startingDate;
+	private LocalDate endingDate;
+    private JButton dayButton;
+    private JButton weekButton;
+    private JButton monthButton;
+    private JButton agendaButton;
+    private JButton fileButton;
 	
+	private static final long serialVersionUID = 1L;
 	
-		final Container contentPane = getContentPane();
-		setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
-	
-
-		JButton dayButton = new JButton("Day");
-		JButton weekButton = new JButton("Week");
-		JButton monthButton = new JButton("Month");
-		JButton agendaButton = new JButton("Agenda");
-		JButton fileButton = new JButton("From File");
-		ActionListener listener = new ActionListener() {
+	/**
+	 * Initialize the interface of the event frame.
+	 * @param dataModel the DataModel object
+	 * @param formatter the EventFormatter object
+	 */
+	public EventFrame(DataModel dataModel, EventFormatter formatter) {
+		this.dataModel = dataModel;
+		this.formatter = formatter;
+		dayButton = new JButton("Day");
+		dayButton.setSelected(true);
+		weekButton = new JButton("Week");
+		monthButton = new JButton("Month");
+		agendaButton = new JButton("Agenda");
+		fileButton = new JButton("From File");
+		result = "";
+		view = "day";
+	    final Container contentPane = getContentPane();
+	    setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
+	    
+	    ActionListener listener = new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				FileDialog fileDialog = new FileDialog(new JFrame());
 				fileDialog.setVisible(true);
 				File[] files = fileDialog.getFiles();
 				Scanner scanner = null;
-				if (files.length > 0) {
+				if(files.length > 0) {
 					try {
-						scanner = new Scanner(files[0]);
-						try {
-							processFile(scanner);
-						} catch (Exception e1) {
-							// TODO Auto-generated catch block
-							e1.printStackTrace();
-						}
-					} catch (FileNotFoundException exception) {
-						exception.printStackTrace();
-					} finally {
-						if (scanner != null) {
-							scanner.close();
-						}
+				    	scanner = new Scanner(files[0]);
+				    	processFile(scanner);
+					}
+					catch (FileNotFoundException exception) {
+					    exception.printStackTrace();
+					} 
+					finally {
+					    if (scanner != null) {
+					        scanner.close();
+					    }
 					}
 					fileButton.setBorderPainted(false);
 					fileButton.removeActionListener(fileButton.getActionListeners()[0]);
 				}
 			}
-		};
-		fileButton.addActionListener(listener);
+	    };
+	    fileButton.addActionListener(listener);
+	    
+	    JButton viewButtons[] = new JButton[] {dayButton,  weekButton, monthButton, agendaButton};
+	    ActionListener viewListener = new ActionListener() {
 
-		ActionListener filterListener = new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if (e.getSource() == dayButton) {
-					showByDay();
-				} else if (e.getSource() == weekButton) {
-					showByWeek();
-				} else if (e.getSource() == monthButton) {
-					showByMouth();
-				} else if (e.getSource() == agendaButton) {
-				//	showByAgenda();
+				JButton button = (JButton) e.getSource();
+				if(button == viewButtons[0]) {
+					setDayView();
 				}
-			}
-		};
-		dayButton.addActionListener(filterListener);
-		weekButton.addActionListener(filterListener);
-		monthButton.addActionListener(filterListener);
-		agendaButton.addActionListener(filterListener);
-
-		JPanel panel1 = new JPanel();
-		panel1.add(dayButton);
-		panel1.add(weekButton);
-		panel1.add(monthButton);
-		panel1.add(agendaButton);
-
-		JPanel panel2 = new JPanel();
-		panel2.add(fileButton);
-
-	//	showByAgenda();
-		add(panel1);
-		add(panel2);
-		add(panel3);
-
-		setLocation(0, 500);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		pack();
-		setVisible(true);
-	}
-
-	/**
-	 * method to get all dates between 2 dates returns ArrayList of LocalDates
-	 * @param startDate
-	 * @param endDate
-	 * @return
-	 * @throws ParseException
-	 */
-	public static ArrayList<LocalDate> datesBetween(LocalDate startDate, LocalDate endDate) throws ParseException {
-		
-		ArrayList<LocalDate> listDatesBetween = new ArrayList<>();
-
-		while (!startDate.isAfter(endDate)) {
-			listDatesBetween.add(startDate);
-			startDate = startDate.plusDays(1);
-		}
-		return listDatesBetween;
-
-	}
-	/**
-	 * gets last day of given month and year
-	 * 
-	 */
-	public static int monthLength(String monthYear) throws ParseException{
-		DateTimeFormatter pattern = DateTimeFormatter.ofPattern("M/yyyy");   
-	YearMonth ym = YearMonth.parse(monthYear,pattern);
-		LocalDate date = ym.atEndOfMonth();
-		return date.lengthOfMonth();
-	}
-	/**
-	 * get all dates with events happening regularly
-	 * 
-	 * @param day: occurrence of events such as "MTWRFSSu"
-	 * @param totalDates
-	 * @return regularDates ArrayList of Dates with regular events
-	 */
-	public static ArrayList<LocalDate> datesWithEvents(String day, ArrayList<LocalDate> totalDates) {
-
-		ArrayList<LocalDate> regularDates = new ArrayList<LocalDate>();
-		for (int i = 0; i < totalDates.size(); i++) {
-			if (day.contains("M")) {
-				if (totalDates.get(i).getDayOfWeek().toString().toLowerCase().equals("monday")) {
-					regularDates.add(totalDates.get(i));
+				else if(button == viewButtons[1]) {
+					setWeekView();
 				}
-			}
-			if (day.contains("T")) {
-				if (totalDates.get(i).getDayOfWeek().toString().toLowerCase().equals("tuesday")) {
-					regularDates.add(totalDates.get(i));
-				}
-			}
-			if (day.contains("W")) {
-				if (totalDates.get(i).getDayOfWeek().toString().toLowerCase().equals("wednesday")) {
-					regularDates.add(totalDates.get(i));
-				}
-			}
-			if (day.contains("H")) {
-				if (totalDates.get(i).getDayOfWeek().toString().toLowerCase().equals("thursday")) {
-					regularDates.add(totalDates.get(i));
-				}
-			}
-			if (day.contains("F")) {
-				if (totalDates.get(i).getDayOfWeek().toString().toLowerCase().equals("friday")) {
-					regularDates.add(totalDates.get(i));
-				}
-			}
-			if (day.contains("A")) {
-				if (totalDates.get(i).getDayOfWeek().toString().toLowerCase().equals("saturday")) {
-					regularDates.add(totalDates.get(i));
-				}
-			}
-			if (day.contains("S")) {
-				if (totalDates.get(i).getDayOfWeek().toString().toLowerCase().equals("sunday")) {
-					regularDates.add(totalDates.get(i));
-				}
-			}
-		}
-
-		return regularDates;
-	}
-	/**
-	 * goes through file
-	 * 
-	 * @param fileScanner
-	 * @throws Exception 
-	 */
-	public void processFile(Scanner fileScanner) throws Exception {
-		while (fileScanner.hasNextLine()) {
-			String token[] = fileScanner.nextLine().split(";");
-			
-			String eventName = token[0];
-			int year = Integer.parseInt(token[1]);
-			int startingMonth = Integer.parseInt(token[2]);
-			int endingMonth = Integer.parseInt(token[3]);
-			String occursOn = token[4];
-			
-			String monthYear = token[3]+"/"+token[1];
-			LocalTime startTime = LocalTime.parse(token[5]);
-			LocalTime endTime = LocalTime.parse(token[6]);
-			TimeInterval timeInterval = new TimeInterval(startTime,endTime);
-			LocalDate startDate = LocalDate.of(year, startingMonth, 1);
-			LocalDate endDate = LocalDate.of(year, endingMonth, monthLength(monthYear));
-			
-			
-			ArrayList<LocalDate> datesWithEventsFile =  datesWithEvents(occursOn, datesBetween(startDate,endDate));
-			for (int i = 0; i < datesWithEventsFile.size(); i++) {
-				Event event = new Event(eventName, timeInterval);
-				if(dataModel.allEvents.containsKey(datesWithEventsFile.get(i))){
-					
-					ArrayList<Event> eventsOnDate = dataModel.allEvents.get(datesWithEventsFile.get(i));
-					eventsOnDate.add(event);
-					
+				else if(button == viewButtons[2]) {
+					setMonthView();
 				}
 				else {
-					ArrayList<Event> newEventList = new ArrayList<Event>();
-					newEventList.add(event);
-					dataModel.allEvents.put(datesWithEventsFile.get(i), newEventList);
-			
+					@SuppressWarnings("unused")
+					AgendaFrame af = new AgendaFrame(calendarFrame.getCurrentClick(), calendarFrame.getEventFrame());
+				}
+				if(button != viewButtons[3]) {
+					for(int i = 0; i < viewButtons.length; i++) {
+						viewButtons[i].setSelected(false);
+					}
+					button.setSelected(true);
 				}
 			}
+	    	
+	    };
+	    for(int i = 0; i < viewButtons.length; i++) {
+	    	viewButtons[i].addActionListener(viewListener);
+	    }
+	    
+	    JPanel panel1 = new JPanel();
+	    panel1.add(dayButton);
+	    panel1.add(weekButton);
+	    panel1.add(monthButton);
+	    panel1.add(agendaButton);
+	    
+	    JPanel panel2 = new JPanel();
+	    panel2.add(fileButton);
+	    
+	    final int row = 30;
+	    final int column = 50;
+	    textArea = new JTextArea(row, column);
+	    textArea.setEditable(false);
+	    JScrollPane scroll = new JScrollPane(textArea);
+	    
+	    
+	    JPanel panel3 = new JPanel();
+	    panel3.add(scroll);
+	    
+	    add(panel1);
+	    add(panel2);
+	    add(panel3);
+	    
+	    setLocation(0, 500);
+	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    pack();
+	    setVisible(true);
+	}
+	
+	/**
+	 * Read events from the file and add them to the data model.
+	 * @param fileScanner the file scanner to be used to read file
+	 */
+	public void processFile(Scanner fileScanner) {
+		while(fileScanner.hasNextLine()) {
+			String events[] = fileScanner.nextLine().split(";");
+			for(int i = 0; i < events.length; i++)
+				events[i] = events[i].trim();
+			dataModel.addEvent(new Event(events[0], Integer.parseInt(events[1]), Integer.parseInt(events[2]), Integer.parseInt(events[3])
+					   				   , events[4], Integer.parseInt(events[5]), Integer.parseInt(events[6])));
+			if(view.equals("day")) {
+				setDayView();
+			}
+			else if(view.equals("week")) {
+				setWeekView();
+			}
+			else if(view.equals("month")){
+				setMonthView();
+			}
+			else {
+				setAgendaView();
+			}
 		}
-		
 	}
-
-	private void showByDay() {
-
-	}
-
-	private void showByWeek() {
-
-	}
-
-	private void showByMouth() {
-		panel3.removeAll();
-		repaint();
-	}
-
-	private void showByAgenda() {
-		panel3.removeAll();
-		String events[] = new String[dataModel.allEvents.size()];
-		for (int i = 0; i < dataModel.allEvents.size(); i++) {
-			events[i] = "Days: " + dataModel.allEvents.get(key) + "; StartingMonth: "
-					+ dataModel.allEvents.get(i).getStartingMonth() + "; EndingMonth: "
-					+ dataModel.allEvents.get(i).getEndingMonth() + "; Year: "
-					+ dataModel.allEvents.get(i).getYear() + "; Name: " + dataModel.allEvents.get(i).getEventName();
-		}
-		JList eventList = new JList<>(events);
-		JScrollPane eventScrollPane = new JScrollPane(eventList);
-		eventScrollPane.setPreferredSize(new Dimension(column, row));
-		panel3.add(eventScrollPane);
-		panel3.revalidate();
-		repaint();
-	}
-
+	
+	/**
+	 * Set the calendar frame reference and current click.
+	 * @param frame the calendar frame
+	 */
 	public void setCalendarFrame(CalendarFrame frame) {
 		this.calendarFrame = frame;
+		current = calendarFrame.getCurrentClick();
 	}
 
+	/**
+	 * Do reaction when the new event is added to the dataModel or set new views.
+	 * @param e the ChangeEvent object
+	 */
 	@Override
 	public void stateChanged(ChangeEvent e) {
-		LocalDate current = calendarFrame.getCurrentClick();
-		ArrayList<Event> events = dataModel.allEvents;
+		ArrayList<Event> events = dataModel.getEvents();
+		current = calendarFrame.getCurrentClick();
 		ArrayList<Event> eventsOnCurrentDay = new ArrayList<>();
-		for (int i = 0; i < events.size(); i++) {
+		for(int i = 0; i < events.size(); i++) {
 			Event event = events.get(i);
-			if (current.getYear() == event.getYear()
-					&& (current.getMonthValue() >= event.getStartingMonth()
-							&& current.getMonthValue() < event.getEndingMonth())
-					&& event.getDays().contains(current.getDayOfWeek().getValue()))
-				eventsOnCurrentDay.add(event);
-			showByAgenda();
+			if(current.getYear() == event.getYear()){
+				if(event.getEndingMonth() == 0) {
+					if(event.getStartingMonth() == current.getMonthValue() && (int) event.getDays().get(0) == current.getDayOfMonth())
+						eventsOnCurrentDay.add(event);
+				}
+				else {
+					if((current.getMonthValue() >= event.getStartingMonth() && current.getMonthValue() <= event.getEndingMonth())
+					   && event.getDays().contains(current.getDayOfWeek().getValue()))
+						eventsOnCurrentDay.add(event);
+				}
+			}
+				
 		}
+		result = dataModel.format(eventsOnCurrentDay, formatter, current);
+	}
+	
+	/**
+	 * Set the text area with new results.
+	 */
+	public void setTextArea() {
+		textArea.setText(result);
+	}
+	
+	/**
+	 * Set the result to empty for other useage. 
+	 */
+	public void setResult() {
+		result = "";
+	}
+	
+	/**
+	 * Get latest result in the textArea.
+	 * @return the latest result.
+	 */
+	public String getResult() {
+		return result;
+	}
+
+	/**
+	 * Get current view
+	 * @return the current view
+	 */
+	public String getView() {
+		return view;
+	}
+	
+	/**
+	 * Set the day view to the text area and calendar frame.
+	 */
+	public void setDayView() {
+		view = "day";
+		calendarFrame.setView(view);
+		stateChanged(null);
+		setTextArea();
+		setResult();
+	}
+	
+	/**
+	 * Set the week view to the text area and calendar frame.
+	 */
+	public void setWeekView() {
+		view = "week";
+		calendarFrame.setView(view);
+		current = calendarFrame.getCurrentClick();
+		LocalDate click = LocalDate.of(current.getYear(), current.getMonth(), current.getDayOfMonth());
+		int dayOfWeek = current.getDayOfWeek().getValue();
+		String weekResult = "";
+		if(dayOfWeek != 7) {
+			for(int i = dayOfWeek; i > 1; i--) {
+				current = current.minusDays(1);
+			}
+			current = current.minusDays(1);
+		}
+		for(int i = 0; i <= 6; i++) {
+			calendarFrame.setCurrentClick(current);
+			stateChanged(null);
+			weekResult += result;
+			current = current.plusDays(1);
+		}
+		current = click;
+		result = weekResult;
+		setTextArea();
+		setResult();
+		calendarFrame.setCurrentClick(click);
+	}
+	
+	/**
+	 * Set the month view to the text area and calendar frame.
+	 */
+	public void setMonthView() {
+		view = "month";
+		calendarFrame.setView(view);
+		current = calendarFrame.getCurrentClick();
+		LocalDate firstDay = LocalDate.of(current.getYear(), current.getMonth(), 1);
+		LocalDate click = LocalDate.of(current.getYear(), current.getMonth(), current.getDayOfMonth());
+		String monthResult = "";
+		int length = firstDay.getMonth().length(firstDay.isLeapYear());
+		for(int i = 0; i < length; i++) {
+			calendarFrame.setCurrentClick(firstDay);
+			stateChanged(null);
+			monthResult += result;
+			firstDay = firstDay.plusDays(1);
+		}
+		result = monthResult;
+		setTextArea();
+		setResult();
+		calendarFrame.setCurrentClick(click);
+	}
+	
+	/**
+	 * Set the agenda view to the text area and calendar frame.
+	 */
+	public void setAgendaView() {
+		LocalDate start = LocalDate.of(startingDate.getYear(), startingDate.getMonth(), startingDate.getDayOfMonth());
+		view = "agenda";
+		current = calendarFrame.getCurrentClick();
+		LocalDate click = LocalDate.of(current.getYear(), current.getMonth(), current.getDayOfMonth());
+		String agendaResult = "";
+		while(startingDate.compareTo(endingDate) <= 0) {
+			calendarFrame.setCurrentClick(startingDate);
+			stateChanged(null);
+			agendaResult += result;
+			startingDate = startingDate.plusDays(1);
+		}
+		result = agendaResult;
+		setTextArea();
+		setResult();
+		calendarFrame.setCurrentClick(click);
+		startingDate = start;
+	}
+	
+	/**
+	 * Set the starting date of the agenda
+	 * @param startingDate the starting date of the agenda
+	 */
+	public void setStartingDate(LocalDate startingDate) {
+		this.startingDate = startingDate;
+	}
+	
+	/**
+	 * Set the ending date of the agenda
+	 * @param endingDate the ending date of the agenda
+	 */
+	public void setEndingDate(LocalDate endingDate) {
+		this.endingDate = endingDate;
 	}
 }
